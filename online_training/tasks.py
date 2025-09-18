2c4820a909cd5fe2269f41d56c2b733a98ab678b
Aleksandr <feat9999@yandex.ru>
2025-09-18 16:36:29 +0400

    new funk

--- online_training/tasks.py
+++ online_training/tasks.py
@@ -13,11 +13,18 @@ def send_information(email):
     """Асинхронная рассылка писем пользователям об обновлении материалов курса"""
     send_mail('Обновление материала', 'Вышло новое обновление', settings.EMAIL_HOST_USER, [email])
 
+# @shared_task
+# def time_block_user():
+#     """Проверка и блокировка неактивных пользователей"""
+#     one_month_ago = timezone.now() - timedelta(days=30)
+#     inactive_users = User.objects.filter(last_login__lt=one_month_ago, is_active=True)
+#     for user in inactive_users:
+#         user.is_active = False
+#         user.save()
+
 @shared_task
 def time_block_user():
     """Проверка и блокировка неактивных пользователей"""
-    one_month_ago = timezone.now() - timedelta(days=30)
-    inactive_users = User.objects.filter(last_login__lt=one_month_ago, is_active=True)
-    for user in inactive_users:
-        user.is_active = False
-        user.save()
+    User.objects.filter(
+        last_login__lt=timezone.now() - timezone.timedelta(days=30), is_active=True
+    ).update(is_active=False)
